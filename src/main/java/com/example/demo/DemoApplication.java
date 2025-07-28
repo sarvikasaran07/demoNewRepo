2de11cefa9419347c1305e935fe336d5981b8a64
Saranya <sarvikasaran07@gmail.com>
2025-07-28 18:34:33 +0400

    git main push

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
+@RestController
 public class DemoApplication {
 
+	@GetMapping("/hello")
+	public String checkMethod() {
+		return "Hello: Saranya";
+	}
+	@GetMapping("/hi")
+	public String checkMethod1() {
+		return "Hello: hi";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
